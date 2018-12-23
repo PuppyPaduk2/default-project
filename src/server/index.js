b320e948f8492d7fb5f1ad34ac55e984c351e07a
puppypaduk2 <debraooclark@gmail.com>
2018-12-23 22:05:16 +0300

    setup .env

--- src/server/index.js
+++ src/server/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import express from 'express';
 import http from 'http';
+import dotenv from 'dotenv';
 import { server as clientServer } from '../client/core/app';
 import Main from '../client/modules/main';
 
-const PORT = 5000;
 const app = express();
 const httpServer = http.Server(app);
+const ENV = dotenv.config().parsed;
 
 app.use(express.json());
-app.use(express.static('dist/client'));
+app.use(express.static(ENV.PATH_STATIC));
 app.get('/favicon.ico', (req, res) => res.sendStatus(404));
 app.get('/', (req, res) => {
   res.send(clientServer(() => <Main />, {
@@ -17,6 +18,6 @@ app.get('/', (req, res) => {
   }));
 });
 
-httpServer.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+httpServer.listen(ENV.PORT, () => {
+  console.log(`Example app listening on port ${ENV.PORT}!`);
 });
