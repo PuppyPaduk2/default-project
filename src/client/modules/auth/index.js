d3009db5a1f587505895e68d20888ec7d44e9f8f
puppypaduk2 <debraooclark@gmail.com>
2018-12-23 23:00:48 +0300

    server to servises

---
+++ src/client/modules/auth/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Content() {
+  console.log('@Auth module');
+
+  return (
+    <div>Auth module</div>
+  );
+}
--- src/server/services/main/index.js
+++ src/server/services/main/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import express from 'express';
 import http from 'http';
 import dotenv from 'dotenv';
-import { server as clientServer } from '../client/core/app';
-import Main from '../client/modules/main';
+import { server as clientServer } from '../../../client/core/app';
+import Main from '../../../client/modules/main';
 
 const app = express();
 const httpServer = http.Server(app);
@@ -12,6 +12,7 @@ const ENV = dotenv.config().parsed;
 app.use(express.json());
 app.use(express.static(ENV.PATH_STATIC));
 app.get('/favicon.ico', (req, res) => res.sendStatus(404));
+
 app.get('/', (req, res) => {
   res.send(clientServer(() => <Main />, {
     modules: ['main'],
